cc132152110101d70dcc6384b348c755869b6639
Gianluca <gianluca.porcino86@gmail.com>
2012-05-04 18:21:12 +0200

    Modifiche varie

--- 20120326/FIX_SIMPLEXGRID.js
+++
@@ -1,4 +0,0 @@
-// FIX PLASM....solo per versione on-line
-SIMPLEXGRID = function (quotes) {
-    return p.simplexGrid(quotes);
-}
--- 20120326/TRASLAZIONE_PLASM.js
+++
@@ -1,4 +0,0 @@
-var a = (T([0,1,2])([a,b,c])(object)); 
-//dove 0 = x, 1 = y, 2 = z
-//ATTENZIONE: gli assi PIANI di PLASM sono x e z, mentre y è la profondità
-//a, b, c sono le quantià DA SOMMARE ALLA PRECEDENTE POSIZIONE DI object
